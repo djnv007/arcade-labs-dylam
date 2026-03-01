131e9a426f07d2ecc0d4e787fa16ea1c6bb445ca
dylam <dylam.nolasco@edu.uah.es>
2026-03-01 20:38:05 +0100

    modified:   lab03-preguntas/lab03-json.py

---
+++ lab03-preguntas/lab03-json.py
@@ -0,0 +1,43 @@
+import json
+lista_preguntas=json.load(open("preguntas.json", encoding="utf-8"))
+import random
+def extraer_pregunta(pregunta: str) -> dict:
+    separador=pregunta.split("|")
+    diccionario={}
+    diccionario["pregunta"]=separador[0]
+    diccionario["correcta"]=separador[1]
+    diccionario["opciones"]=separador[1:]
+    random.shuffle(diccionario["opciones"])
+    return diccionario
+def letras_opciones(longitud: int) -> list:
+    letras=[]
+    for i in range(longitud):
+        letras.append(chr(97+i))
+    return letras
+def validar_respuestas(opciones):
+    valido=False
+    while not valido:
+        try:
+            respuesta=input("Introduce tu respuesta: ")
+            if respuesta not in opciones:raise
+            else: return opciones.index(respuesta)
+        except:
+            print("Respuesta no válida, intenta de nuevo")
+        
+def juego(lista_preguntas: list) -> None:
+    puntos=0
+    for pregunta in range(len(lista_preguntas)):
+        print(f"{lista_preguntas[pregunta]["pregunta"]}")
+        opciones= lista_preguntas[pregunta]["opciones"]
+        letras=letras_opciones(len(opciones))
+        for opcion in (range(len(opciones))):
+            print(f"{letras[opcion]}) {opciones[opcion]}")
+        respuesta=validar_respuestas(letras)
+        if lista_preguntas[pregunta]["opciones"][respuesta]==lista_preguntas[pregunta]["correcta"]:
+            print("¡Respuesta correcta!")
+            puntos+=1
+        else:
+            print("¡Respuesta incorrecta!")
+    print(f"Puntos obtenidos: {puntos}")
+        
+juego(lista_preguntas)                
